6d3b2a9a70271da86dc663a70ccb4c07e9bd04f5
rafaelasterg <103337174+rafaelasterg@users.noreply.github.com>
2024-02-15 15:59:01 +0200

    Android

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "gr.hua.dit.android.geofence"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "gr.hua.dit.android.geofence"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        vectorDrawables.useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildToolsVersion = "34.0.0"
+}
+
+dependencies {
+    implementation ("com.google.maps.android:android-maps-utils:3.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-common:2.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+}
